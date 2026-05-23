ac2fcb233a6892c8c3308242a0d59798a44fbd22
hayashikeita13 <hayashi-keita@sagri.co.jp>
2026-05-23 16:53:40 +0900

    feat(page): ホームページの雛形を実装

---
+++ src/frontend/components/templates/HomeTemplate/index.tsx
@@ -0,0 +1,9 @@
+import styles from './HomeTemplate.module.scss';
+
+export default function HomeTemplate() {
+  return (
+    <div className={styles.root}>
+      <p>home template</p>
+    </div>
+  );
+}
