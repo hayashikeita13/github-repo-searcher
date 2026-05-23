ca3d58a6378c0062775f49ceb8541b3f71084a7e
hayashikeita13 <hayashi-keita@sagri.co.jp>
2026-05-23 15:30:52 +0900

    nextプロジェクトのテンプレートを作成

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +13,22 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "jsx-quotes": ["error", "prefer-single"],
+      quotes: ["error", "single", { avoidEscape: true }],
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "es5",
+          tabWidth: 2,
+          singleQuote: true,
+          jsxSingleQuote: true,
+          printWidth: 120,
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
