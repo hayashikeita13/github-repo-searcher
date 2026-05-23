39a87cd0a85cab2e72980a965949f14e8a9fb3a2
hayashikeita13 <hayashi-keita@sagri.co.jp>
2026-05-23 17:50:57 +0900

    feat(Header): タイトルをホームリンクとするヘッダーコンポーネントを実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Header from '@/frontend/components/organisms/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -22,8 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className='min-h-full flex flex-col'>{children}</body>
+    <html lang='js' className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
